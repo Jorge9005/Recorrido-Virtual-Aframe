9995a3e99b5f53f974a9e58b3dc60b0d7d30ff6c
Jorge Sandoval <jorge9005@outlook.com>
2023-04-28 12:54:06 -0600

    Direccionamiento de imagenes 360
    Direccioné todas las imágenes 360

--- assets/js/components/js.js
+++ assets/js/components/js.js
@@ -1,46 +1,35 @@
-$('.message a').click(function(){
-    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
- });
+$('.message a').click(function () {
+    $('form').animate({ height: "toggle", opacity: "toggle" }, "slow");
+});
 
- function info()
- {
-    if(document.getElementById("container-info").style.visibility == "hidden")
-    {
-        for(i=1;i<1000;i++)
-    {
-        document.getElementById("container-info").style.visibility = "visible";
+function info() {
+    if (document.getElementById("container-info").style.visibility == "hidden") {
+        for (i = 1; i < 1000; i++) {
+            document.getElementById("container-info").style.visibility = "visible";
+        }
     }
+    else {
+        for (i = 1; i < 1000; i++) {
+            document.getElementById("container-info").style.visibility = "hidden";
+        }
     }
-    else
-    {
-        for(i=1;i<1000;i++)
-    {
-        document.getElementById("container-info").style.visibility = "hidden";
-    }
-    }
-   // document.getElementById("container-menu").style.visibility = "visible";
- }
+    // document.getElementById("container-menu").style.visibility = "visible";
+}
 
- function menu()
- {
-    if(document.getElementById("container-menu").style.visibility == "hidden")
-    {
-        for(i=1;i<1000;i++)
-    {
-        document.getElementById("container-menu").style.visibility = "visible";
+function menu() {
+    if (document.getElementById("container-menu").style.visibility == "hidden") {
+        for (i = 1; i < 1000; i++) {
+            document.getElementById("container-menu").style.visibility = "visible";
+        }
     }
+    else {
+        for (i = 1; i < 1000; i++) {
+            document.getElementById("container-menu").style.visibility = "hidden";
+        }
     }
-    else
-    {
-        for(i=1;i<1000;i++)
-    {
-        document.getElementById("container-menu").style.visibility = "hidden";
-    }
-    }
- }
+}
 
- function onh()
- {
+function onh() {
     menu();
     info();
- }
+}
