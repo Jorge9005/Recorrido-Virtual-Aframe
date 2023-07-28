f9b66b23255cb5b8da6104e4c9400097b5171db7
Jorge Sandoval <92350041+Jorge9005@users.noreply.github.com>
2023-07-27 23:33:17 -0600

    Delete Recursos/Ejemplos directory

--- Recursos/Ejemplos/Ejemplo Rain/index.js
+++
@@ -1,9 +0,0 @@
-function toggleRain() {
-    let a = document.getElementById('a');
-    if (a.hasAttribute('rain')) {
-      a.removeAttribute('rain');
-    } else {
-      a.setAttribute('rain', '1');
-    }
-    console.log ("ok");
-  }
